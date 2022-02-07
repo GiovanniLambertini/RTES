49b105fbbfab49a6779f1d1d0c58c36e66bb2501
GiovanniLambertini <71648823+GiovanniLambertini@users.noreply.github.com>
2022-02-07 18:18:43 +0100

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+ 
+
+void main(int argc, char *argv[]) {
+
+    int i,j,n;
+
+
+
+    n = atoi (argv[1]);
+
+    
+
+    printf("I numeri primi sono:-\n");    
+
+    for(i=2;i<=n;i++) {
+
+        int numDivisori=0;
+
+        for(j=1;j<=i;j++) {
+
+            if(i%j==0) {
+
+                numDivisori++;
+
+            }
+
+        }
+
+         
+
+        if(numDivisori==2) {
+
+            printf("%d ",i);
+
+        }
+
+    }
+
+    printf("\n");
+
+}
+
+
+

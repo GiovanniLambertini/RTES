41032e9c315c1e8d0f7b22cdd1fa3cfb4169c96f
GiovanniLambertini <71648823+GiovanniLambertini@users.noreply.github.com>
2022-02-07 22:50:30 +0100

    Update sort.c

--- sort.c
+++ sort.c
@@ -9,14 +9,14 @@ int main (int argc, char **argv) {
     for (i = 1; i < argc; i++) {
         min_index=i;
 
-        for (j = i+1; j < argc; j++) {
+        for (j = i+1; j < argc; j++) 
             if (strcmp(argv[min_index] , argv[j]) > 0)
                   min_index=j;
 
-            temp = argv[i];
-            argv[i] = argv[min_index];
-            argv[min_index] = temp;
-     }
+        temp = argv[i];
+        argv[i] = argv[min_index];
+        argv[min_index] = temp;
+    }
 
     for (i = 0; i < argc; i++)
         printf("%s ", argv[i]);  

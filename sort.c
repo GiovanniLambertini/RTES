9d530e657e5bb91ecced20fae5a80f69880e9593
GiovanniLambertini <71648823+GiovanniLambertini@users.noreply.github.com>
2022-02-07 22:46:21 +0100

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char **argv) {  
+    int i, j, min_index;
+    char* temp;
+
+    for (i = 1; i < argc; i++) {
+        min_index=i;
+
+        for (j = i+1; j < argc; j++) {
+            if (strcmp(argv[min_index] , argv[j]) > 0)
+                  min_index=j;
+
+            temp = argv[i];
+            argv[i] = argv[min_index];
+            argv[min_index] = temp;
+     }
+
+    for (i = 0; i < argc; i++)
+        printf("%s ", argv[i]);  
+
+    printf ("\n");
+
+    return 0;  
+}
